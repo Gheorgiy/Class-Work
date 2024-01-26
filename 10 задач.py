7326efae6fd5238b1f1097c463eb4e1d872e0290
donnmeeee <101360433+Gheorgiy@users.noreply.github.com>
2024-01-26 11:59:40 +0300

    Add files via upload

---
+++ 10 задач.py
@@ -0,0 +1,65 @@
+    #задача 1
+    
+x = int(input("введите число: "))
+y = 7 * x + 5
+print("Ответ: y = ", y )
+
+    #Задача 2
+    
+a = int(input("Сторона квадрата: "))
+b = a * 4 
+print("Ответ: b = ", b )
+
+    #Задача 3
+    
+r = int(input("Радиус: "))
+c = 2 * 3.14 * r 
+print("Ответ: ", c )
+
+    #Задача 4
+    
+d = int(input("Диаметр окружности: "))
+s = 3.14 * (d/2) ** 2
+print("Ответ: ", s )
+
+    #Задача 5
+    
+a1 = int(input("Первый катет: "))
+b1 = int(input("Второй катет: "))
+sqrt = (a1 ** 2 + b1 ** 2)
+print("Ответ: ",sqrt )
+
+    #Задача 6 
+a2 = int(input("Катет1: "))
+a3 = int(input("Катет2: "))
+sqrt = (a2 ** 2 + a3 ** 2)
+print("Ответ: ", sqrt )
+
+    #Задача 7 
+wall = int(input("Сторона1: "))
+wall2 = int(input("Сторона2: "))
+paper = wall * 2 + wall2 * 2 
+sqrt5 = (wall ** 2 + wall2 ** 2)
+print("Периметр: ", paper )
+print("Диагональ: ", sqrt5 )
+
+    #Задача 8 
+r1 = int(input("Радиус внешней окружности "))
+r2 = int(input("Радиус внутренней окружности "))
+s1 = 3.14 * r1 ** 2 - 3.14 * r2 ** 2 
+print("Площадь кольца: ", s1 )
+
+    #Задача 9 
+rebro = int(input("Ребро куба "))
+spoverhnosti = 6 * rebro ** 2
+v = rebro ** 2 
+print("Площадь поверхности: ", spoverhnosti )
+print("Объем куба: ", v )
+
+    #Задача 10 
+trapetia1 = int(input("Высота "))
+trapetia2 = int(input("верхнее основание "))
+trapetia3 = int(input("Нижнее основание "))
+per = trapetia1 * ((trapetia2 + trapetia3 )/ 2 )
+print("Периметр: ", per )
+
