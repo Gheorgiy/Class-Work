b5ad38aa7974cd636bc9ea0bc410a8db18134811
donnmeeee <101360433+Gheorgiy@users.noreply.github.com>
2024-02-02 11:41:29 +0300

    02.02.2024

---
+++ практическая работа2.py
@@ -0,0 +1,12 @@
+idea = 10
+idea2 = 5
+
+print(idea > 9 and not idea2 > 10) 
+print(idea == 10 and idea2 >= 5)
+print(idea < 2 and idea2 > 28)
+print(idea2 != 5 and idea > 33)
+
+print(not idea > 9 or idea2 < 10) 
+print(idea == 10 or idea2 >= 5)
+print(not idea > 2 or not idea2 < 28)
+print(idea2 != 5 or idea > 33)
