40c3f3d90d2d46acffb811b515cfc86f0de666ab
donnmeeee <101360433+Gheorgiy@users.noreply.github.com>
2024-02-02 11:04:46 +0300

    02.02.2024 практическая работа

---
+++ Практическая работа.py
@@ -0,0 +1,10 @@
+#Задача
+tseloe = 10 
+drobnoe = 8.4
+stroka = "No"
+big_netseloe = tseloe * 3.5 
+drobnoe = drobnoe - 1 
+print(tseloe / drobnoe, big_netseloe / drobnoe)
+stroka1 = "Yes"
+print(stroka * 2 + "_" + stroka1 * 3)
+print(tseloe, drobnoe, stroka, big_netseloe, stroka1)
