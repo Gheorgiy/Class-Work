d723e2279a783c5c8ff2bb64ea5fcabbe5ace07c
donnmeeee <101360433+Gheorgiy@users.noreply.github.com>
2024-02-16 10:58:12 +0300

    16.02.2024

--- практическая 2.py
+++ практическая 2.py
@@ -1,17 +1,47 @@
-idea = 10
-idea2 = 5
-
-print(idea > 9 and not idea2 > 10) 
-print(idea == 10 and idea2 >= 5)
-print(idea < 2 and idea2 > 28)
-print(idea2 != 5 and idea > 33)
-
-print(not idea > 9 or idea2 < 10) 
-print(idea == 10 or idea2 >= 5)
-print(not idea > 2 or not idea2 < 28)
-print(idea2 != 5 or idea > 33)
-
-stroka = "26"
-stroka2 = "36"
-c = 5
-print(stroka > "0" and stroka2 != "26")
+#задача 1 
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+if a > b:
+    print(a, " - max", b, " - min")
+elif a < b:
+    print(b, " - max", a, " - min")
+else:
+    print("Числа равны")
+    
+#задача 2
+r = int(input("Введите радиус круга: "))
+s = int(input("Введите сторону квадрата: "))
+r = 2 * r
+if r < s :
+    print("Впишется")
+else:
+    print("Не впишется")
+    
+#задача 3
+x = int(input("Введите х: "))
+if x < 0:
+    print( "y = x **2" )
+elif x > 0:
+    print("y = 1/(x**2)")
+else:
+    print("x = 0")
+    
+#задача 4
+k = int(input("Введите радиус круга: "))
+l = int(input("Введите сторону квадрата: "))
+l = 2 * l
+if l > k :
+    print("Впишется")
+else:
+    print("Не впишется")
+    
+#задача 5
+o = int(input("Введите первое число: "))
+p = int(input("Введите второе число: "))
+if o > p:
+    print(o, " - max")
+elif o < p:
+    print(p, " - max")
+else:
+    print("Числа равны")
+    
