b1ae5b58db0268d588104ba9bb5b629844c70eed
donnmeeee <101360433+Gheorgiy@users.noreply.github.com>
2024-04-02 16:06:17 +0300

    Add files via upload

---
+++ задачи с функциями2.py
@@ -0,0 +1,10 @@
+def swed ():
+    is_year_leap = input("Напишите число в году ")
+    if is_year_leap == 366:
+        print("Год високосный")
+    else:
+        print("Год не високосный")
+        
+c, d = swed ()
+e, f = swed ()
+z, x = swed ()
